77e0f83e373e3e23430a51df769c724bb03bffd0
Caio Peres <caiofperes@hotmail.com>
2025-08-21 19:47:00 -0300

    Initial project commit

---
+++ app/src/main/java/com/example/dynamicui/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dynamicui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.dynamicui.ui.theme.DynamicUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DynamicUITheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "MainScreen") {
+                    composable("MainScreen") {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DynamicUITheme {
+        Greeting("Android")
+    }
+}
